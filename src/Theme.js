b769ec6b60422caaed32b3de37a4e55defb3b952
Henry-Z <chancidy@gmail.com>
2017-08-04 13:35:01 -0400

    cant remember

--- src/Theme.js
+++ src/Theme.js
@@ -7,6 +7,7 @@ const validDomain = "asu.edu";
 //Colors for a particular theme
 const Primary = '#0048a5';
 const Secondary = "#ffd200";
+const Success = "#ffd200";
 const DeleteColor = "#8c1d40";
 const NavColor = "#353535";
 

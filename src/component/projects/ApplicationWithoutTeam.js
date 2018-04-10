159672e0de9728916eb9f63b2f1b5006149dade1
Sampaleen <spaleen@udel.edu>
2018-04-10 15:59:27 -0400

    General Team Inquiry Form Avalible on front page

--- src/component/projects/ApplicationWithoutTeam.js
+++ src/component/projects/ApplicationWithoutTeam.js
@@ -2,15 +2,19 @@ import React, {Component} from 'react';
 import firebase from '../../firebase';
 import _ from 'lodash';
 import {checkEmpty} from '../../Validation';
+import {Link} from 'react-router-dom';
 
 //Material ui Components
 import {Card, CardTitle} from 'material-ui/Card';
 import Checkbox from 'material-ui/Checkbox';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import MenuItem from 'material-ui/MenuItem';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import SelectField from 'material-ui/SelectField';
 import TextField from 'material-ui/TextField';
+
 
 //Components 
 import MuiButton from '../MuiButton';
@@ -40,8 +44,14 @@ const style = {
     display:'flex',
     justifyContent:'center',
     flexDirection:'column'
+  },
+  text:{
+    alignSelf:'center'
   }
 }
+
+//variables
+const TeamFormPath = 'StudentApplication_Raw_Data';
 
 class ApplicationWithoutTeam extends Component{
   constructor(){
@@ -94,7 +104,7 @@ class ApplicationWithoutTeam extends Component{
       data['credits'] = '';
       data['level'] = '';
       data['returning'] = 'false';
-      data['team'] = '';
+      data['teamName'] = '';
       
       this.setState({
         formQuestions:snap.val(),
@@ -135,7 +145,7 @@ class ApplicationWithoutTeam extends Component{
   handleCourseChange(event, index, value){
     let data = this.state.data;
     console.log(value);
-    data['course'] = this.state.courses[this.state.data['team']][value].course;
+    data['course'] = this.state.courses[this.state.data['teamName']][value].course;
     this.setState({
       data:data,
       courseValue:value
@@ -144,7 +154,7 @@ class ApplicationWithoutTeam extends Component{
 
   handleMenuChange(event, index, value){
     let data = this.state.data;
-    data['team'] = this.state.teams[value].teamName;
+    data['teamName'] = this.state.teams[value].teamName;
     this.setState({
       data:data,
       value:value
@@ -154,10 +164,17 @@ class ApplicationWithoutTeam extends Component{
   handleSubmit(){
     console.log(this.state.data);
     let validation = checkEmpty(this.state.error, this.state.data, this.state.data['email'], this.state.notIncluded);
-    this.setState({
-      error:validation[1],
-      errorEmail:validation[2]
-    });
+    if(validation[0]){
+      firebase.database().ref(`${TeamFormPath}`).push(this.state.data);
+      this.setState({
+        open:true
+      });
+    }else{
+      this.setState({
+        error:validation[1],
+        errorEmail:validation[2],
+      });
+    }
   }
 
   handleTextChange(event, index, value){
@@ -173,6 +190,15 @@ class ApplicationWithoutTeam extends Component{
     let formQuestions = this.state.formQuestions;
     let levels = this.state.levels;
     let teams = this.state.teams;
+
+    const actions = [
+      <Link to = '/'><FlatButton
+        label="Close"
+        primary={true}
+        onClick={this.handleClose}
+      /></Link>,
+    ];
+
     return(
       <div>
         <MuiThemeProvider>
@@ -188,6 +214,7 @@ class ApplicationWithoutTeam extends Component{
                               floatingLabelText={formQuestions[question].text} 
                               value = {this.state.data[formQuestions[question].id]}
                               id = {formQuestions[question].id}
+                              style = {style.text}
                               errorText = {this.state.errorEmail} 
                               onChange = {this.handleTextChange}
                             />
@@ -197,46 +224,57 @@ class ApplicationWithoutTeam extends Component{
                                 floatingLabelText={formQuestions[question].text} 
                                 value = {this.state.data[formQuestions[question].id]} 
                                 id = {formQuestions[question].id}
+                                style = {style.text}
                                 errorText = {this.state.error[formQuestions[question].id]} 
                                 onChange = {this.handleTextChange}
                               />
                     }
                   })}
-                  <SelectField floatingLabelText="Select a Team" value={this.state.value} onChange={this.handleMenuChange}  errorText={this.state.error['team']}>
-                    {Object.keys(teams).map((team)=>(
-                      <MenuItem value = {team} primaryText={teams[team].teamName} />
-                    ))
-                    }
-                  </SelectField>
-                  <SelectField floatingLabelText="Select Your Level" value={this.state.levelValue} onChange={this.handleLevelChange} errorText={this.state.error['level']}>
-                    {levels.map((level, index)=>(
-                      <MenuItem value = {index} primaryText={level} key = {index}/>
-                    ))
-                    }
-                  </SelectField>
-                  {this.state.data['team'] &&
-                    <div>
-                      <SelectField floatingLabelText="Select A Course" value={this.state.courseValue} onChange={this.handleCourseChange} errorText={this.state.error['course']}>
-                        {Object.keys(courses[this.state.data['team']]).map((course, index)=>{
-                          return <MenuItem value = {course} primaryText={courses[this.state.data['team']][course].course} key = {index}/>;
+                  <div style = {{display:'flex', justifyContent:'center', flexDirection:'column'}}>
+                    <SelectField floatingLabelText="Select a Team" value={this.state.value} onChange={this.handleMenuChange}  errorText={this.state.error['team']} style = {style.text}>
+                      {Object.keys(teams).map((team)=>(
+                        <MenuItem value = {team} primaryText={teams[team].teamName} />
+                      ))
+                      }
+                    </SelectField>
+                    <SelectField floatingLabelText="Select Your Level" value={this.state.levelValue} onChange={this.handleLevelChange} errorText={this.state.error['level']} style = {style.text}>
+                      {levels.map((level, index)=>(
+                        <MenuItem value = {index} primaryText={level} key = {index}/>
+                      ))
+                      }
+                    </SelectField>
+                  </div>
+                  {this.state.data['teamName'] &&
+                    <div style = {{display:'flex', justifyContent:'center', flexDirection:'column'}}>
+                      <SelectField floatingLabelText="Select A Course" value={this.state.courseValue} onChange={this.handleCourseChange} errorText={this.state.error['course']} style = {style.text}>
+                        {Object.keys(courses[this.state.data['teamName']]).map((course, index)=>{
+                          return <MenuItem value = {course} primaryText={courses[this.state.data['teamName']][course].course} key = {index}/>;
                         })
                         }
                       </SelectField>
-                      <h4 style = {{color:'#acb1b4'}}>Select Credits</h4>
-                      <RadioButtonGroup valueSelected = {this.state.credit} onChange = {this.handleCreditChange}>
+                      <h4 style = {{color:'#acb1b4', alignSelf:'center'}}>Select Credits</h4>
+                      <RadioButtonGroup valueSelected = {this.state.credit} onChange = {this.handleCreditChange} style = {style.text}>
                         {this.state.creditOptions.map((option)=>(
                           <RadioButton value = {option} label = {option} key = {option} style = {style.radioButton} />
                         ))}
                       </RadioButtonGroup>
                     </div>
                   }
-                  <Checkbox label="Check if you're returning to VIP" checked={this.state.returning} style = {{marginTop:'20px', marginBottom:'20px'}} onCheck = {this.handleCheck} />
+                    <Checkbox label="Check if you're returning to VIP" checked={this.state.returning} style = {{marginTop:'20px', marginBottom:'20px', alignSelf:'center', width:'256px'}} onCheck = {this.handleCheck} />
                 </div>
               </Card>
               }
             </div>
         </MuiThemeProvider>
         <MuiButton label = "Apply" style = {style.submit} onClick = {this.handleSubmit}/>
+        <Dialog
+          title="Applied!"
+          actions={actions}
+          modal={true}
+          open={this.state.open}
+        >
+          Thank you for applying!
+        </Dialog>
       </div>);
   }
 }
